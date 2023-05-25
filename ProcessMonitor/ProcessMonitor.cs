95f9fc6f5692dc19b312f9e3d05f0d1b2fe62fd1
immengzi <13017606416@163.com>
2023-05-26 01:38:43 +0800

    添加锁屏时暂停记录

--- ProcessMonitor/ProcessMonitor.cs
+++ ProcessMonitor/ProcessMonitor.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
+using Microsoft.Win32;
 
 namespace ProcessMonitor
 {
@@ -14,52 +15,74 @@ namespace ProcessMonitor
         private string activeProcessName;
         private Stopwatch stopwatch;
         private List<Record> records;
+        private bool isMonitoring;
 
         public ProcessMonitor()
         {
             activeProcessName = GetActiveProcessName(); // 初始化为当前活动的软件名称
             stopwatch = new Stopwatch();
             records = new List<Record>();
+            isMonitoring = true;
         }
 
         public void StartMonitoring()
         {
+            SystemEvents.SessionSwitch += OnSessionSwitch;
             while (true)
             {
-                var currentSoftwareName = GetActiveProcessName();
-
-                if (currentSoftwareName != activeProcessName && currentSoftwareName != "Idle")
+                if (isMonitoring)
                 {
-                    var elapsed = stopwatch.Elapsed;
-
-                    var existingRecord = records.FirstOrDefault(r => r.ProcessName == activeProcessName);
-
-                    if (existingRecord != null)
                     {
-                        existingRecord.Duration += elapsed;
+                        var currentSoftwareName = GetActiveProcessName();
+
+                        if (currentSoftwareName != activeProcessName && currentSoftwareName != "Idle")
+                        {
+                            var elapsed = stopwatch.Elapsed;
+
+                            var existingRecord = records.FirstOrDefault(r => r.ProcessName == activeProcessName);
+
+                            if (existingRecord != null)
+                            {
+                                existingRecord.Duration += elapsed;
+                            }
+                            else
+                            {
+                                records.Add(new Record(activeProcessName, elapsed));
+                            }
+
+                            records = records.OrderByDescending(r => r.Duration).ToList();
+
+                            Console.WriteLine("应用使用时长记录：");
+                            foreach (var record in records)
+                            {
+                                var formattedDuration = DurationFormatter.FormatDuration(record.Duration);
+                                Console.WriteLine($"应用: {record.ProcessName}, 使用时长: {formattedDuration}");
+                                record.FormattedDuration = formattedDuration;
+                            }
+
+                            CsvWriter.WriteRecordsToCsv(records, "Record.csv");
+
+                            stopwatch.Restart();
+                            activeProcessName = currentSoftwareName;
+                        }
+
+                        Thread.Sleep(10);
                     }
-                    else
-                    {
-                        records.Add(new Record(activeProcessName, elapsed));
-                    }
-
-                    records = records.OrderByDescending(r => r.Duration).ToList();
-
-                    Console.WriteLine("应用使用时长记录：");
-                    foreach (var record in records)
-                    {
-                        var formattedDuration = DurationFormatter.FormatDuration(record.Duration);
-                        Console.WriteLine($"应用: {record.ProcessName}, 使用时长: {formattedDuration}");
-                        record.FormattedDuration = formattedDuration;
-                    }
-
-                    CsvWriter.WriteRecordsToCsv(records, "Record.csv");
-
-                    stopwatch.Restart();
-                    activeProcessName = currentSoftwareName;
                 }
-
-                Thread.Sleep(10);
+            }
+            
+        }
+        private void OnSessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                isMonitoring = false;
+                Console.WriteLine("记录已暂停!");
+            }
+            else if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                isMonitoring = true;
+                Console.WriteLine("记录已恢复~");
             }
         }
 
