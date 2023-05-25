42f0376d63e88e1f7e27d18598cbedf5c2b6ac40
immengzi <13017606416@163.com>
2023-05-26 01:23:37 +0800

    初次提交
    完成基本功能

---
+++ ProcessMonitor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProcessMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processMonitorMonitor = new ProcessMonitor();
+            processMonitorMonitor.StartMonitoring();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProcessMonitor/Record.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProcessMonitor
+{
+    class Record
+    {
+        public string ProcessName { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string FormattedDuration { get; set; }
+        public DateTime Date { get; set; }
+
+        public Record(string processName, TimeSpan duration)
+        {
+            ProcessName = processName;
+            Duration = duration;
+            Date = DateTime.Now;
+        }
+    }
+}
